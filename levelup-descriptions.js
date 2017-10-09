3d42f9ea3a7bf501ab9c717207d5bb9d1d7b515d
Eric Brandwein <brandweineric@gmail.com>
2017-10-09 18:09:24 -0300

    Funciones para resolver los hit dice y hit points
    Las funciones reemplazan los valores de los dados y los nombres de las clases dependiendo de la clase que reciben

--- levelup-descriptions.js
+++ levelup-descriptions.js
@@ -1,22 +1,52 @@
 var fs = require('fs');
 
-var DESCRIPTIONS_DIR = './descriptions'
+var DESCRIPTIONS_DIR = './descriptions/'
 
 Classes = {
-    BARBARIAN: 'barbarian',
-    BARD: 'bard',
-    WIZARD: 'wizard'
+    BARBARIAN: {
+        name: 'barbarian',
+        hitDie: 'd12',
+        hitPointIncreaseAverage: 7
+    },
+    BARD: {
+        name: 'bard',
+        hitDie: 'd8',
+        hitPointIncreaseAverage: 5
+    },
+    WIZARD: {
+        name: 'wizard',
+        hitDie: 'd6',
+        hitPointIncreaseAverage: 4
+    }
 }
 
 
-function get(characterClass, characterLevel, callback) {
-    getHitDiceIncreaseDescription((err, data) => {
-        callback(err, getDescriptionPage(data));
+function get(characterClass, characterLevel, callback){
+    var description;
+    getHitDiceIncreaseDescription(characterClass, (err, data) => {
+        description = data + '\n';
+        getHitPointsIncreaseDescription(characterClass, (err, data) => {
+            description += data + '\n';
+            callback(err, getDescriptionPage(description));
+        });
     });
 }
 
-function getHitDiceIncreaseDescription(callback){
-    fs.readFile(DESCRIPTIONS_DIR + '/hit-dice-increase.html', callback);
+function getHitDiceIncreaseDescription(characterClass, callback){
+    fs.readFile(DESCRIPTIONS_DIR + 'hit-dice-increase.html', (err, data) => {
+        var newData = String(data).replace("%die%", characterClass.hitDie);
+        callback(err, newData);
+    });
+}
+
+function getHitPointsIncreaseDescription(characterClass, callback){
+    fs.readFile(DESCRIPTIONS_DIR + 'hit-points-increase.html', (err, data) => {
+        var newData = String(data).replace('%die%', characterClass.hitDie);
+        newData = newData.replace('%average%',
+            characterClass.hitPointIncreaseAverage);
+        newData = newData.replace('%class%', characterClass.name);
+        callback(err, newData);
+    });
 }
 
 function getDescriptionPage(description){
