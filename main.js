8d0970142fc99b876482ba55ea6dabd4b327fc7d
Eric Brandwein <brandweineric@gmail.com>
2017-10-15 16:11:42 -0300

    Adds a form at the beggining of every page
    So as to go to the page of your class and level.

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-var descriptions = require('./levelup-descriptions.js')
-
-app.get('/:className/:level', function (req, res) {
-    var characterClass = descriptions.getClassByName(req.params.className);
-    console.log(req.params.className);
-    console.log(characterClass);
-    descriptions.get(characterClass, req.params.level, (err, data) => {
-        if (err) throw err;
-        console.log(data);
-        res.end(data);
-    });
-});
+var descriptions = require('./levelup-descriptions.js');
 
 app.get('/', function(req, res) {
-    descriptions.get(descriptions.Classes.BARBARIAN, 2, (err, data) => {
+    var clazz = descriptions.getClassByName(req.query.class);
+    var level = parseInt(req.query.level);
+    descriptions.get(clazz, level, (err, data) => {
         if (err) throw err;
-        console.log(data);
         res.end(data);
     });
 });
