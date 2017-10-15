17362b889487e620dc4e28a220215b94ba8276a3
Eric Brandwein <brandweineric@gmail.com>
2017-10-15 16:16:17 -0300

    Checks to see if there are queryparams

--- main.js
+++ main.js
@@ -4,8 +4,19 @@ var app = express();
 var descriptions = require('./levelup-descriptions.js');
 
 app.get('/', function(req, res) {
-    var clazz = descriptions.getClassByName(req.query.class);
-    var level = parseInt(req.query.level);
+    var clazz = req.query.class;
+    if (clazz) {
+        clazz = descriptions.getClassByName(req.query.class);
+    } else {
+        clazz = descriptions.Classes.BARBARIAN;
+    }
+    var level = req.query.level;
+    if (level) {
+        level = parseInt(req.query.level);
+    } else {
+        level = 2;
+    }
+
     descriptions.get(clazz, level, (err, data) => {
         if (err) throw err;
         res.end(data);
