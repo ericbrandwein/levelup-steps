155920a1703cc0c5d9a6f425b35ec329ed9fb474
Eric Brandwein <brandweineric@gmail.com>
2017-10-16 17:44:26 -0300

    Redirects to a default page when missing parameters

--- main.js
+++ main.js
@@ -4,23 +4,31 @@ var app = express();
 var descriptions = require('./levelup-descriptions.js');
 
 app.get('/', function(req, res) {
+    var shouldRedirect = false;
+
     var clazz = req.query.class;
     if (clazz) {
         clazz = descriptions.getClassByName(req.query.class);
     } else {
         clazz = descriptions.Classes.BARBARIAN;
+        shouldRedirect = true;
     }
     var level = req.query.level;
     if (level) {
         level = parseInt(req.query.level);
     } else {
         level = 2;
+        shouldRedirect = true;
     }
 
-    descriptions.get(clazz, level, (err, data) => {
-        if (err) throw err;
-        res.end(data);
-    });
+    if (shouldRedirect) {
+        res.redirect('/?class=' + clazz.name + '&level=' + level);
+    } else {
+        descriptions.get(clazz, level, (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    }
 });
 
 app.listen(8080);
