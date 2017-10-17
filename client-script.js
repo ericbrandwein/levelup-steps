21b59249b7f3a427f7acde014e66803870b79398
Eric Brandwein <brandweineric@gmail.com>
2017-10-17 16:46:22 -0300

    Adds some classes additional info
    The bard, cleric, fighter, rogue and wizard. The additional infos are added to the client-script.js

--- client-script.js
+++ client-script.js
@@ -8,11 +8,64 @@ $(document).ready(function() {
 
 const classesWithAdditionalInfo = {
     barbarian: {
-        title: 'Primal Path:',
-        fromLevel: 3,
+        title: 'Primal Path',
+        fromLevel: 4,
         options: [
             {name: 'Path of the Berserker', value: 'berserker'},
             {name: 'Path of the Totem Warrior', value: 'totem-warrior'}
+        ]
+    },
+    bard: {
+        title: 'Bard College',
+        fromLevel: 3,
+        options: [
+            {name: 'College of Lore', value: 'lore'},
+            {name: 'College of Valor', value: 'valor'}
+        ]
+    },
+    cleric: {
+        title: 'Divine Domain',
+        fromLevel: 1,
+        options: [
+            {name: 'Knowledge', value: 'knowledge'},
+            {name: 'Life', value: 'life'},
+            {name: 'Light', value: 'light'},
+            {name: 'Nature', value: 'nature'},
+            {name: 'Tempest', value: 'tempest'},
+            {name: 'Trickery', value: 'trickery'},
+            {name: 'War', value: 'war'}
+        ]
+    },
+    fighter: {
+        title: 'Martial Archetype',
+        fromLevel: 3,
+        options: [
+            {name: 'Champion', value: 'champion'},
+            {name: 'Battle Master', value: 'battle-master'},
+            {name: 'Eldritch Knight', value: 'eldritch-knight'}
+        ]
+    },
+    rogue: {
+        title: 'Roguish Archetype',
+        fromLevel: 3,
+        options: [
+            {name: 'Thief', value: 'thief'},
+            {name: 'Assassin', value: 'assassin'},
+            {name: 'Arcane Trickster', value: 'arcane-trickster'}
+        ]
+    },
+    wizard: {
+        title: 'Arcane Tradition',
+        fromLevel: 2,
+        options: [
+            {name: 'Abjuration', value: 'abjuration'},
+            {name: 'Conjuration', value: 'conjuration'},
+            {name: 'Divination', value: 'divination'},
+            {name: 'Enchantment', value: 'enchantment'},
+            {name: 'Evocation', value: 'evocation'},
+            {name: 'Illusion', value: 'illusion'},
+            {name: 'Necromancy', value: 'necromancy'},
+            {name: 'Transmutation', value: 'transmutation'}
         ]
     }
 };
@@ -27,9 +80,9 @@ function showAdditionalInfoInputIfNecessary() {
     for (var className in classesWithAdditionalInfo) {
         var classAttributes = classesWithAdditionalInfo[className];
         if (selectedClass === className &&
-            selectedLevel >= classAttributes.fromLevel) {
+            selectedLevel > classAttributes.fromLevel) {
 
-            additionalInfoLabel.html(classAttributes.title);
+            additionalInfoLabel.html(classAttributes.title + ':');
             additionalInfoLabel.show();
 
             additionalInfoInput.html('');
