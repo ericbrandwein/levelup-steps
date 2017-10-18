418d18f4a6b6fe9746be85043b34710c5ca10ed0
Eric Brandwein <brandweineric@gmail.com>
2017-10-18 20:21:35 -0300

    Adds savant feature

--- src/paths/classes/wizard/wizard-paths.js
+++ src/paths/classes/wizard/wizard-paths.js
@@ -1,7 +1,41 @@
 var pathClasses = require('../../path-classes.js');
+var pug = require('pug');
 
-var abjuration = new pathClasses.Path('School of Abjuration');
-var projectedWard = new pathClasses.Feature('Projected Ward', __dirname + '/projected-ward.pug');
+/**
+    A Path that comes with the savant feature already added.
+*/
+class ArcaneTradition extends pathClasses.Path {
+    constructor(school) {
+        super('School of ' + school);
+        this.school = school;
+    }
+
+    renderLevelDescriptions(level, callback) {
+        this._renderSavantFeatureIfNecessary(level, (err, html) => {
+            var description = html;
+            super.renderLevelDescriptions(level, (err, html) => {
+                callback(err, description + html);
+            });
+        });
+    }
+
+    _renderSavantFeatureIfNecessary(level, callback) {
+        if (level === 2) {
+            pug.renderFile(
+                __dirname + '/savant-feature.pug',
+                {school: this.school.toLowerCase(),
+                    path: this.name, feature: this.school + ' Savant'},
+                callback
+            );
+        } else {
+            callback(undefined, '');
+        }
+    }
+}
+
+var abjuration = new ArcaneTradition('Abjuration');
+var projectedWard =
+    new pathClasses.Feature('Projected Ward', __dirname + '/projected-ward.pug');
 abjuration.addFeature(6, projectedWard);
 
 module.exports = {
