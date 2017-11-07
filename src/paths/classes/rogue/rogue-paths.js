95f6e1a7c92ee20ae0103dab97f0605bed9a7a33
Eric Brandwein <brandweineric@gmail.com>
2017-11-07 00:30:30 -0300

    Adds spellcasting arcane trickster feature

--- src/paths/classes/rogue/rogue-paths.js
+++ src/paths/classes/rogue/rogue-paths.js
@@ -1,7 +1,12 @@
 var pathClasses = require('../../path-classes.js');
+var utils = require('../../path-classes-utils.js');
+
+function getFeature(title, descriptionFile) {
+    return new pathClasses.Feature(title, __dirname + descriptionFile);
+}
 
 function newFeature(path, level, title, descriptionFile) {
-    var feature = new pathClasses.Feature(title, __dirname + descriptionFile);
+    var feature = getFeature(title, descriptionFile);
     path.addFeature(level, feature);
 }
 
@@ -22,9 +27,10 @@ newFeature(assassin, 13, 'Impostor', '/assassin/impostor.pug');
 newFeature(assassin, 17, "Death Strike", '/assassin/death-strike.pug');
 
 var arcaneTrickster = new pathClasses.Path('Arcane Trickster');
-// Esto se enquilomba porque cada nivel hay diferentes spell slots y cosas.
-newFeature(arcaneTrickster, 3,
-    'Spellcasting', '/arcane-trickster/spellcasting.pug');
+var spellcasting = getFeature('Spellcasting', '/arcane-trickster/spellcasting.pug');
+var spellcastingLevels =
+    [3, 4, 7, 8, 10, 11, 13, 14, 16, 19, 20];
+utils.addFeatureInLevels(arcaneTrickster, spellcasting, spellcastingLevels);
 newFeature(arcaneTrickster, 3,
     'Mage Hand Legerdemain', '/arcane-trickster/mage-hand-legerdemain.pug');
 newFeature(arcaneTrickster, 9,
